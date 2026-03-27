ab9ee06a37e54fd5c8275ab602d72a3beac4a189
John <johneliud4@gmail.com>
2026-03-27 11:53:34 +0300

    feat(movie-service): add Movie entity with Neo4j node mapping, auto-generated UUID, and rating fields.

---
+++ src/main/java/io/github/johneliud/movie_service/entity/Movie.java
@@ -0,0 +1,43 @@
+package io.github.johneliud.movie_service.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.support.UUIDStringGenerator;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Node("Movie")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie {
+
+    @Id
+    @GeneratedValue(generatorClass = UUIDStringGenerator.class)
+    private String id;
+
+    private String title;
+
+    private List<String> genres;
+
+    private Integer releaseYear;
+
+    private String description;
+
+    private String posterUrl;
+
+    private Double averageRating;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
