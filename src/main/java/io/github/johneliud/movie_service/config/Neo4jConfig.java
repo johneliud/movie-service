e8687d01da88c6c318f06cba21c2564b24f699c9
John <johneliud4@gmail.com>
2026-03-29 17:47:14 +0300

    feat(movie-service): enable Neo4j auditing for automatic timestamp management.

---
+++ src/main/java/io/github/johneliud/movie_service/config/Neo4jConfig.java
@@ -0,0 +1,9 @@
+package io.github.johneliud.movie_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.neo4j.config.EnableNeo4jAuditing;
+
+@Configuration
+@EnableNeo4jAuditing
+public class Neo4jConfig {
+}
